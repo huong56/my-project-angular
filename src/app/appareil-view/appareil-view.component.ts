696bdece2778c07b18276b80e0d4031ca2188537
Thanh Huong Tran <tranhuong56@gmail.com>
2019-03-20 17:03:15 +0100

    first commit

---
+++ src/app/appareil-view/appareil-view.component.ts
@@ -0,0 +1,62 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AppareilService} from '../services/appareil.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-appareil-view',
+  templateUrl: './appareil-view.component.html',
+  styleUrls: ['./appareil-view.component.scss']
+})
+export class AppareilViewComponent implements OnInit, OnDestroy {
+
+  isAuth = false;
+  @Input() id: number;
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+        () => {
+          resolve(date);
+        }, 2000
+    );
+  });
+
+  appareils: any[];
+  appareilSubscription: Subscription;
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+        () => {
+          this.isAuth = true;
+        }, 4000
+    );
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.appareilService.switchOffAll();
+    } else {
+      return null;
+    }
+
+
+  }
+
+  ngOnInit(): void {
+    this.appareilSubscription = this.appareilService.appareilsSubject.subscribe(
+        (appareils: any[]) => {
+          this.appareils = appareils;
+        }
+    );
+    this.appareilService.emitAppareilSubject();
+  }
+
+  ngOnDestroy(): void {
+    this.appareilSubscription.unsubscribe();
+  }
+
+}
